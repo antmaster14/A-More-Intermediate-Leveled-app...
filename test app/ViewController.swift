b23be1c961d2609ae78f240abbbac95cd04622c8
Anthony Turner <anthonytechturner@sheldons-mbp.lan>
2017-08-02 16:38:27 -0500

    Chapter 1.) UI Views 			picker views
    just finished this, committing for safety reasons...👌😇

--- test app/ViewController.swift
+++ test app/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let items:[String] = ["item 1", "item 2", "item 3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return items.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return items[row]
     }
 
     override func didReceiveMemoryWarning() {
